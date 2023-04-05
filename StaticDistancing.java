d3f5b4433e2568f125258f506784d43823dea39c
Rez_Colin <ironcolin12@gmail.com>
2023-04-05 12:12:15 +0800

    Static Distancing
    ur mum

---
+++ StaticDistancing.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+public class StaticDistancing{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the location you would like to enter");
+
+        String user_input = input.nextLine();
+        System.out.println("Username is: " + user_input);  // Output user input
+    }
+}
