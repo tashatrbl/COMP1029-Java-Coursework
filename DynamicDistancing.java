05719697829ccf7a8ea9a004d85006082012d70b
tashatrbl <carmelbarnabas@gmail.com>
2023-04-12 00:42:21 +0800

    Updated DynamicDistancing

--- DynamicDistancing.java
+++ DynamicDistancing.java
@@ -1,4 +1,37 @@
+import java.lang.Math;
+
 public class DynamicDistancing {
-    //write down a dynamic distancing class here
-    
+    public int[] currentCapacities;
+    public int OPMainAreaCurrCapacity;
+    public int OPSubAreaCurrCapacity;
+    public int ICUCurrCapacity;
+    public int IPMainAreaCurrCapacity;
+
+    public int[] getCurrCapacity() {
+        int[] maxCapacities = StaticDistancing.getMaxCapacity();
+        currentCapacities = new int[maxCapacities.length];
+
+        for (int i = 0; i < maxCapacities.length; i++) {
+            int capacity = (int) (Math.random() * (maxCapacities[i] + 2));
+            switch (i) {
+                case 0:
+                    OPMainAreaCurrCapacity = capacity;
+                    break;
+                case 1:
+                    OPSubAreaCurrCapacity = capacity;
+                    break;
+                case 2:
+                    ICUCurrCapacity = capacity;
+                    break;
+                case 3:
+                    IPMainAreaCurrCapacity = capacity;
+                    break;
+                default:
+                    break;
+            }
+            currentCapacities[i] = capacity;
+        }
+
+         return currentCapacities;
+    }
 }
