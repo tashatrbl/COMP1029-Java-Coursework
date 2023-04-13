4fb591621e31e94ce083c1e69b23b6610bb0d34c
Adyan Dean <adyandean05@gmail.com>
2023-04-13 23:09:13 +0800

    Pushy?

--- RestrictedSpots.java
+++ RestrictedSpots.java
@@ -1,9 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class RestrictedSpots{
     public static void main(String[] args) {
         restrictedSpots h = new restrictedSpots();
-        RestrictedSpot research_center = new RestrictedSpot(2, "Research Center", "", 50, 20);
         Main_waiting_area foo = new Main_waiting_area(2, "balls", "arf", 4, 2);
         System.out.println(foo.getSpotID());
+
+        restrictedSpots research_center = new Research_center(2, "Research Center", "", 50, 20);
+        restrictedSpots surgery_room = new Surgery_room(3, "Surgery Room", "", 120, 5);
+        
+        Scanner input = new Scanner(System.in);
+        int choice;
+        
+        do {
+            System.out.println("Enter 2 for Research Center or 3 for Surgery Room, or 0 to exit:");
+            choice = input.nextInt();
+            
+            if (choice == 2) {
+                int randomCapacity = research_center.Spot_Maximum_Capacity() + (int)(Math.random() * 10) + 1;
+                System.out.println("Spot Name: " + research_center.Spot_Name());
+                System.out.println("Spot ID: " + research_center.getSpotID());
+                System.out.println("Random Maximum Capacity: " + randomCapacity);
+                System.out.println("Permitted Average Time: " + research_center.Spot_Permitted_Average_Time());
+                System.out.println("Random Capacity: " + ((Research_center)research_center).getSpotRandomCapacity());
+            } else if (choice == 3) {
+                int randomCapacity = surgery_room.Spot_Maximum_Capacity() + (int)(Math.random() * 10) + 1;
+                System.out.println("Spot Name: " + surgery_room.Spot_Name());
+                System.out.println("Spot ID: " + surgery_room.getSpotID());
+                System.out.println("Random Maximum Capacity: " + randomCapacity);
+                System.out.println("Permitted Average Time: " + surgery_room.Spot_Permitted_Average_Time());
+            }
+            
+        } while (choice != 0);
     }
 
     // Method / Function
@@ -13,13 +42,15 @@ public class RestrictedSpots{
         private String Spot_Area;
         private int Spot_Permitted_Average_Time;
         private int Spot_Maximum_Capacity;
+        private Random random;
 
         public restrictedSpots(int Spot_ID, String Spot_Name, String Spot_area, int Spot_Permitted_Average_Time, int Spot_Maximum_Capacity){
             this.Spot_ID = Spot_ID;
             this.Spot_Name = Spot_Name;
             this.Spot_Area = Spot_Area;
             this.Spot_Permitted_Average_Time = Spot_Permitted_Average_Time;
-            this.Spot_Maximum_Capacity = Spot_Maximum_Capacity;
+            this.random = new Random();
+            this.Spot_Maximum_Capacity = Spot_Maximum_Capacity + random.nextInt(11);
         }
 //comment
         public restrictedSpots(){
@@ -29,10 +60,6 @@ public class RestrictedSpots{
             // main_waiting_area.Spot_area="";
             // main_waiting_area.Spot_Permitted_Average_Time=50;
             // main_waiitng_area.Spot_Maximum_Capacity=30;
-
-            // RestrictedSpots research_center = new RestrictedSpots();
-            // RestrictedSpots surgery_area = new RestrictedSpots();
-            // RestrictedSpots discharge_lounge = new RestrictedSpots();
         }
 
         public int getSpotID(){
@@ -56,7 +83,7 @@ public class RestrictedSpots{
         }
     }
 
-    public static class Main_waiting_area extends RestrictedSpot {
+    public static class Main_waiting_area extends restrictedSpots {
         
         public Main_waiting_area(int spotID, String spotName, String spotArea, int spotPermittedAverageTime, int spotMaximumCapacity) {
             super(spotID, spotName, spotArea, spotPermittedAverageTime, spotMaximumCapacity);
@@ -68,11 +95,26 @@ public class RestrictedSpots{
     }
 
     // Subclass for Research Center
-    public static class Research_center extends RestrictedSpot {
+    public static class Research_center extends restrictedSpots {
+        
+        private int Spot_Random_Capacity;
         
         public Research_center(int spotID, String spotName, String spotArea, int spotPermittedAverageTime, int spotMaximumCapacity) {
             super(spotID, spotName, spotArea, spotPermittedAverageTime, spotMaximumCapacity);
+            Random r = new Random();
+            this.Spot_Random_Capacity = r.nextInt(10) + 1;
+        }
+        
+        public int getSpotRandomCapacity() {
+            return Spot_Random_Capacity;
+        }
+
+    }
+
+    public static class Surgery_room extends restrictedSpots {
+        
+        public Surgery_room(int spotID, String spotName, String spotArea, int spotPermittedAverageTime, int spotMaximumCapacity) {
+            super(spotID, spotName, spotArea, spotPermittedAverageTime, spotMaximumCapacity);
         }
     }
-}
 }
