c9b190d374d5eab5bd7f598aa9c74200030f95e5
tashatrbl <carmelbarnabas@gmail.com>
2023-04-17 20:10:43 +0800

    Simplified/removed unnecessary code

--- DynamicDistancing.java
+++ DynamicDistancing.java
@@ -2,12 +2,7 @@ import java.lang.Math;
 
 public class DynamicDistancing {
     public int[] currentCapacities;
-    public static int OPMainAreaCurrCapacity;
-    public static int OPSubAreaCurrCapacity;
-    public static int ICUCurrCapacity;
-    public static int IPMainAreaCurrCapacity;
-    public static int RCCurrCapacity;
-    public static int SRCurrCapacity;
+
 
     public int[] setCurrCapacity(int user_input) {
         int[] maxCapacities = StaticDistancing.getMaxCapacity(user_input);
@@ -15,63 +10,9 @@ public class DynamicDistancing {
 
         for (int i = 0; i < maxCapacities.length; i++) {
             int capacity = (int) (Math.random() * (maxCapacities[i] + 1));
-            switch (i) {
-                case 0:
-                    OPMainAreaCurrCapacity = capacity;
-                    break;
-                case 1:
-                    OPSubAreaCurrCapacity = capacity;
-                    break;
-                case 2:
-                    ICUCurrCapacity = capacity;
-                    break;
-                case 3:
-                    IPMainAreaCurrCapacity = capacity;
-                    break;
-                case 4:
-                    RCCurrCapacity = capacity;
-                    break;
-                case 5:
-                    SRCurrCapacity = capacity;
-                    break;
-                default:
-                    break;
-            }
             currentCapacities[i] = capacity;
         }
         return currentCapacities;
-    }
-
-    public int[] getCurrCapacity(int user_input) {
-        int[] maxCapacities = StaticDistancing.getMaxCapacity(user_input);
-        int[] currCapacityArr = new int[maxCapacities.length];
-
-        for (int i = 0; i < currentCapacities.length; i++) {
-            switch (i) {
-                case 1:
-                    currCapacityArr[i] = OPMainAreaCurrCapacity;
-                    break;
-                case 2:
-                    currCapacityArr[i] = OPSubAreaCurrCapacity;
-                    break;
-                case 3:
-                    currCapacityArr[i] = ICUCurrCapacity;
-                    break;
-                case 4:
-                    currCapacityArr[i] = IPMainAreaCurrCapacity;
-                    break;
-                case 5:
-                    currCapacityArr[i] = RCCurrCapacity;
-                    break;
-                case 6:
-                    currCapacityArr[i] = SRCurrCapacity;
-                    break;
-                default:
-                    break;
-            }
-        }
-        return currCapacityArr;
-
     }
 
 }
